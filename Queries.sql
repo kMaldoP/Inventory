166fa470f9965b43edd2087c6884fc799206e5b8
krysmaldonado <krysm2015@gmail.com>
2023-03-16 01:02:54 -0700

    Added Queries

--- Queries.sql
+++ Queries.sql
@@ -4,24 +4,37 @@ CREATE TABLE products(
     quantity INT,
 	size TEXT NOT NULL,
 	category TEXT NOT NULL,
+	halfcost MONEY,
 	distributor TEXT NOT NULL
 );
 
 
 
-
-
-
 --Queries-------------------------------------------------------------------------
 
---Slect name and quantity by ditributor and category--
-SELECT name,quantity FROM products
+--Slect name and quantity by distributor and category--
+SELECT name FROM products
 WHERE distributor = '' AND category = '';
 --Select name and quantity by distributor--
-SELECT name,quantity FROM products
+SELECT name FROM products
 WHERE distributor = '';
+--Selecting name, modprice and category in ascending order
+SELECT name, halfcost, category FROM products
+ORDER BY halfcost ASC;
+--grouping total items by halfcost amount
+SELECT COUNT (halfcost)
+FROM products
+WHERE halfcost = MONEY('number here')
+--Selecting Items by category in ascending order
+SELECT name, category, halfcost FROM products
+ORDER BY category, Halfcost ASC
+--Selecting Items by category and ascenging order ommitting null values
+SELECT name, category, halfcost FROM products
+WHERE halfcost IS NOT NULL
+ORDER BY category, Halfcost ASC
+--Display all records with halfcost null values by id,category,grouped by distributor
+SELECT id,name, distributor,category FROM PRODUCTS
+WHERE halfcost IS NULL
+ORDER BY category 
 
-
-
-
-DELETE FROM products
+DELETE FROM products:
